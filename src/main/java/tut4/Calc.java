3496f453bba8422e62dc6cfa797c46c52189bafa
zqy-1129 <3090705852@qq.com>
2021-09-20 16:02:43 +0800

    first commit

---
+++ src/main/java/tut4/Calc.java
@@ -0,0 +1,16 @@
+package tut4;
+
+/**
+ * Hello world!
+ *
+ */
+public class Calc
+{
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public int subtract(int a, int b) {
+        return a+b;
+    }
+}
---
+++ src/test/java/tut4/TestCalc.java
@@ -0,0 +1,15 @@
+package tut4;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TestCalc {
+
+    Calc calc = new Calc();
+
+    @Test
+    void add() {
+        assertEquals(calc.add(1,2), 3);
+    }
+}
